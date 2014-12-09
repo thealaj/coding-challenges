113386106d5bdd02a2baf374903df5acca6c29d3
Alexandria A. Johnson <alexandria.abdallah@gmail.com>
2014-12-09 09:27:20 -0600

    Initial tests passing

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,8 +1,17 @@
 
 var words = function() {};
 
-words.prototype.hey = function(input) {
-
+words = function(input) {
+  input_array = input.split(/\s/)
+  word_hash = {}
+  for (var i = 0; i < input_array.length; i++) {
+    if (word_hash[input_array[i]]) {
+    word_hash[input_array[i]] += 1
+  } else {
+    word_hash[input_array[i]] = 1
+  }
+  }
+  return word_hash
 };
 
 module.exports = words;
