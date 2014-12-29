1a83d4f8c0caf56f327641e463ceebc5ba393907
Alexandria A. Johnson <alexandria.abdallah@gmail.com>
2014-12-29 10:07:07 -0600

    Completed hamming challenge and learned about JavaScript forEach function

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,7 +1,27 @@
-var hamming = function() {};
+var Hamming = function() {};
 
-hamming.prototype.compute = function(input) {
-  return input
-};
 
-module.exports = hamming;
+
+var Hamming = function() {};
+
+Hamming.compute = function(strand1, strand2) {
+  diff = 0
+  var strand1_arr = strand1.split("")
+  var strand2_arr = strand2.split("")
+
+  if (strand1_arr.length != strand2_arr.length) {
+    throw 'DNA strands must be of equal length.'
+  } else {
+    strand1_arr.forEach(function(value, index){
+      if (value !== strand2_arr[index]){
+        diff += 1;
+      }
+    });
+      return diff;
+   };
+
+}
+
+
+
+module.exports = Hamming;
